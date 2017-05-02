b5d7a49aec2fdf5a46dcadea2b4bca52ce1149e3
AlexyVercruysse <alexyvercruysse@gmail.com>
2017-05-02 17:43:17 +0200

    first commit

---
+++ Assets/destroyBallOnContact.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class destroyBallOnContact : MonoBehaviour {
+
+
+	// Use this for initialization
+	int score;
+	void Start () {
+		score = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnTriggerEnter(Collider col){
+		Destroy (col.gameObject);
+		score++;
+	}
+	void OnGUI(){
+		GUI.Box(new Rect(10, 10, 200, 20), "Score : "+score);
+	}
+
+
+}
---
+++ Assets/forceBalleBlanhe.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class forceBalleBlanhe : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GameObject ball0 = GameObject.Find ("Ball0");
+		Rigidbody rigid = ball0.GetComponent<Rigidbody> ();
+		rigid.AddForce (new Vector3 (750,0,0));
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
